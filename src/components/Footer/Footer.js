192379cfeaabf1d6d73966e282f54748bd1834c7
irsalathe <irsalathe@gmail.com>
2023-08-25 22:58:10 -0400

    ain

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://bellasalathe.github.io'
+      href='https://irsalathe.github.io/bellasalathe.github.io/'
       className='link footer__link'
     >
       Created By Bella Salathé
